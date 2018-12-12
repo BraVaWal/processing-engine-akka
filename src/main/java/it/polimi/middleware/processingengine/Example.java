c946f9c7bfa10cda52983e6551cbc569c81f4dcd
Bram van Walraven <b.vanwalraven@student.tudelft.nl>
2018-12-12 16:43:49 +0100

    Create example

---
+++ src/main/java/it/polimi/middleware/processingengine/Example.java
@@ -0,0 +1,59 @@
+package it.polimi.middleware.processingengine;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import it.polimi.middleware.processingengine.operator.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Example {
+
+    public static void main(String[] args) {
+        final ActorSystem system = ActorSystem.create("System");
+
+        ActorRef sink = createWorker(system, new ArrayList<>(), new SinkOperator());
+        ActorRef map = createWorker(system, Collections.singletonList(sink), new MapOperator(message -> {
+            StringBuilder sb = new StringBuilder(message.getValue());
+            int start = message.getValue().indexOf("poli");
+            int end = start + 8;
+            sb.insert(start, "--->");
+            sb.insert(end, "<---");
+            return new Message(message.getKey(), sb.toString());
+        }));
+
+        ActorRef merge = createWorker(system, Collections.singletonList(map), new MergeOperator());
+        ActorRef filter1 = createWorker(system, Collections.singletonList(merge), new FilterOperator(message -> message.getValue().contains("poli")));
+        ActorRef filter2 = createWorker(system, Collections.singletonList(merge), new FilterOperator(message -> message.getValue().contains("poli")));
+        ActorRef filter3 = createWorker(system, Collections.singletonList(merge), new FilterOperator(message -> message.getValue().contains("poli")));
+        ActorRef filter4 = createWorker(system, Collections.singletonList(merge), new FilterOperator(message -> message.getValue().contains("poli")));
+        ActorRef filter5 = createWorker(system, Collections.singletonList(merge), new FilterOperator(message -> message.getValue().contains("poli")));
+        ActorRef filter6 = createWorker(system, Collections.singletonList(merge), new FilterOperator(message -> message.getValue().contains("poli")));
+        ActorRef filter7 = createWorker(system, Collections.singletonList(merge), new FilterOperator(message -> message.getValue().contains("poli")));
+        ActorRef filter8 = createWorker(system, Collections.singletonList(merge), new FilterOperator(message -> message.getValue().contains("poli")));
+
+        StreamGenerator generator1 = new StreamGenerator(filter1, 1, 32, 1000);
+        StreamGenerator generator2 = new StreamGenerator(filter2, 1, 32, 1000);
+        StreamGenerator generator3 = new StreamGenerator(filter3, 1, 32, 1000);
+        StreamGenerator generator4 = new StreamGenerator(filter4, 1, 32, 1000);
+        StreamGenerator generator5 = new StreamGenerator(filter5, 1, 32, 1000);
+        StreamGenerator generator6 = new StreamGenerator(filter6, 1, 32, 1000);
+        StreamGenerator generator7 = new StreamGenerator(filter7, 1, 32, 1000);
+        StreamGenerator generator8 = new StreamGenerator(filter8, 1, 32, 1000);
+
+        new Thread(generator1, "Generator1").start();
+        new Thread(generator2, "Generator2").start();
+        new Thread(generator3, "Generator3").start();
+        new Thread(generator4, "Generator4").start();
+        new Thread(generator5, "Generator5").start();
+        new Thread(generator6, "Generator6").start();
+        new Thread(generator7, "Generator7").start();
+        new Thread(generator8, "Generator8").start();
+    }
+
+    private static ActorRef createWorker(ActorSystem system, List<ActorRef> downstreamWorkers, Operator operator) {
+        return system.actorOf(Worker.props(downstreamWorkers, operator));
+    }
+
+}
