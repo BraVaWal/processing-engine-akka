9a6a01e4e023ab11fc9917358ca8d353e3166a65
Bram van Walraven <b.vanwalraven@student.tudelft.nl>
2018-12-12 15:32:18 +0100

    Add aggregate operator

---
+++ src/main/java/it/polimi/middleware/processingengine/operator/AggregateOperator.java
@@ -0,0 +1,54 @@
+package it.polimi.middleware.processingengine.operator;
+
+import it.polimi.middleware.processingengine.Message;
+import it.polimi.middleware.processingengine.Worker;
+import it.polimi.middleware.processingengine.function.AggregateFunction;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AggregateOperator extends Operator {
+
+    private final AggregateFunction aggregateFunction;
+
+    private final int windowSize;
+    private final int windowSlide;
+
+    private final Map<String, List<String>> windows;
+
+    public AggregateOperator(Worker parent, AggregateFunction aggregateFunction, int windowSize, int windowSlide) {
+        super(parent);
+        this.aggregateFunction = aggregateFunction;
+        this.windowSize = windowSize;
+        this.windowSlide = windowSlide;
+        this.windows = new HashMap<>();
+    }
+
+    @Override
+    public void operate(Message message) {
+        addToWindow(message);
+        if(windowIsFull(message.getKey())) {
+            Message result = aggregateFunction.aggregate(message.getKey(), getWindow(message.getKey()));
+            tell(result);
+            slideWindow(message.getKey());
+        }
+    }
+
+    private void addToWindow(Message message) {
+        getWindow(message.getKey()).add(message.getValue());
+    }
+
+    private List<String> getWindow(String key) {
+        return windows.computeIfAbsent(key, k -> new ArrayList<>(windowSize));
+    }
+
+    private boolean windowIsFull(String key) {
+        return getWindow(key).size() == windowSize;
+    }
+
+    private void slideWindow(String key) {
+        windows.put(key, getWindow(key).subList(windowSlide, windowSize));
+    }
+}
